a0284d42a4aa6c0c9aa0ab26b6e26d0d2f4f9a85
wayhome25 <siwabada@gmail.com>
2017-05-01 17:06:51 +0900

    admin 커스터마이즈

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text'] # 필드 순서 변경
+    fieldsets = [                            # 각 필드 분리하여 표시
+        ('질문내용', {'fields': ['question_text']}),
+        ('작성시간', {'fields': ['pub_date'], 'classes': ['collapse']}), # 필드 접기
+    ]
+    inlines = [ChoiceInline] # Choice 모델 같이 보기
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
