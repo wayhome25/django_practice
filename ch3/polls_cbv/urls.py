e98ea21ba80a1a38003dde3f8dc45270c05ea0da
wayhome25 <siwabada@gmail.com>
2017-05-02 12:45:36 +0900

    polls_cbv.views.vote 뷰 구현

--- ch3/polls_cbv/urls.py
+++ ch3/polls_cbv/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'), # /polls_cbv/
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'), # /polls_cbv/5
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'), # /polls_cbv/5/
+    url(r'^(?P<pk>\d+)/vote/$', views.vote, name='vote'), # /polls_cbv/5/vote/
 ]
--- ch3/polls_cbv/views.py
+++ ch3/polls_cbv/views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView, DetailView
 from .models import Question, Choice
 
 
-#--- ListView
+#--- CBV : ListView
 #--- 객체들이 들어있는 리스트를 컨텍스트 변수에 담아서 템플릿에 넘긴다.
 #--- 리스트가 테이블의 모든 레코드인 경우 모델 클래스만 지정 필요
 class IndexView(ListView):
@@ -12,7 +15,7 @@ class IndexView(ListView):
     # 템플릿 파일 : 모델명 소문자_list.html (question_list.html)
 
 
-#--- DetailView
+#--- CBV : DetailView
 #--- 특정한 객체 하나를 컨텍스트 변수에 담아서 템플릿에 넘긴다.
 #--- 조회시 사용할 Primary Key 값은 URLconf에서 추출하여 뷰로 넘어온 파라미터를 사용한다.
 class DetailView(DetailView):
@@ -21,3 +24,22 @@ class DetailView(DetailView):
     context_object_name = 'question' # 컨텍스트 디폴트 변수명 object => question 으로 변경
     # 컨텍스트 변수 : object (모델의 특정 레코드, 조회용 pk는 urlConf에서 추출)
     # 템플릿 파일 : 모델명 소문자_detail.html (question_detail.html)
+
+
+#--- FBV
+def vote(request, pk):
+    q = get_object_or_404(Question, pk = pk)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+    except:
+        # 설문 투표 폼을 다시 보여준다.
+        return render(request, 'polls_cbv/detail.html', {
+            'question': q,
+            'error_message': '에러발생, 다시 선택해 주세요.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 정상적으로 처리하였으면
+        # 항상 HttpResponseRedirect 를 반환하여 리다이렉션 처리
+        return HttpResponseRedirect(reverse('polls_cbv:results', args=(q.id,)))
