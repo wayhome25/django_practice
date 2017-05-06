dd3b31af10e8b2d00a8653a42ce41e6fc73e20f8
wayhome25 <siwabada@gmail.com>
2017-05-07 00:09:33 +0900

    bookmark.models.Bookmark 모델 생성/admin 등록

--- mysite3/bookmark/admin.py
+++ mysite3/bookmark/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bookmark
 
-# Register your models here.
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+	list_display = ['title', 'url']
