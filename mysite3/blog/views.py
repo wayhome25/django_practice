d930e49947db8d3e20d74e3a23a8899df18df691
wayhome25 <siwabada@gmail.com>
2017-05-07 15:54:09 +0900

    blog.views 뷰 로직 구현

--- mysite3/blog/views.py
+++ mysite3/blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 
-# Create your views here.
+from blog.models import Post
+
+
+#--- List View
+class PostLV(ListView):
+	model = Post
+	template_name = 'blog/post_all.html' # 디폴트 post_list.html
+	context_object_name = 'posts' # 디폴트 objects_list
+	paginate_by = 2
+
+
+#--- Detail View
+class PostDV(DetailView):
+	model = Post
+
+
+#--- Archive View
+class PostAV(ArchiveIndexView):
+	model = Post
+	date_field = 'modify_date'
+
+
+class PostYAV(YearArchiveView):
+	model = Post
+	date_field = 'modify_date'
+	make_objects_list = True
+
+
+class PostMAV(MonthArchiveView):
+	model = Post
+	date_field = 'modify_date'
+
+
+class PostDAV(DayArchiveView):
+	model = Post
+	date_field = 'modify_date'
+
+
+class PostTAV(TodayArchiveView):
+	model = Post
+	date_field = 'modify_date'
