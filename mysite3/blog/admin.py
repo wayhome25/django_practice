6343785a45dffd9bc5eeaef6e705886c1d170a1d
wayhome25 <siwabada@gmail.com>
2017-05-07 14:35:24 +0900

    blog 앱 생성, blog.models.Post 모델 추가 installed app, admin 사이트 등록, migrate

---
+++ mysite3/blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from blog.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'modify_date']
+	list_filter = ['modify_date']
+	search_fields = ['title', 'content']
+	prepopulated_fields = {'slug': ('title',)}
